483c7293bc6035a13f8c795c4f78b37b117612f7
Chizaram Chibueze <chizy7@gmail.com>
2022-11-27 08:37:34 -0500

    Google sign in

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -4,6 +4,7 @@
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import './profiles.dart';
 import './auth.dart';
 import 'firebase_options.dart';
@@ -13,7 +14,11 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
