13736a3478470e7177794a3745bd0f84f5db1396
PC <plinio.cabrera07@myhunter.cuny.edu>
2022-11-30 00:51:45 -0500

    bug fix

--- my_app/lib/user_profile.dart
+++ my_app/lib/user_profile.dart
@@ -1,33 +1,24 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-    const HomePage({super.key});
+  const HomePage({super.key});
 
-    @override
-    State<HomePage> createState() => _HomePage();
+  @override
+  State<HomePage> createState() => _HomePage();
 }
 
 class _HomePage extends State<HomePage> {
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            body: SingleChildScrollView(
-                child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 34),
-                    child: Column(
-                        children: [
-                            Row(
-                                mainAxisAlignment: mainAxisAlignment.spaceBetween,
-                                children: [
-                                    Icon(
-                                        Icons.add_circle_outline
-                                    )
-                                ]
-                            )
-                        ]
-                    )
-                )
-            )
-        )
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+            child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 34),
+                child: Column(children: [
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [Icon(Icons.add_circle_outline)])
+                ]))));
+  }
 }
