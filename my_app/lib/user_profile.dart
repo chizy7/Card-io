bdf19bd7dcf2afc0a9142530d5acfd7ac75e69dc
Abu Butt <abubutt578@gmail.com>
2022-12-01 01:56:28 -0500

    Added google cloud sql database connection and dennies worked on profile screen UI

--- my_app/lib/user_profile.dart
+++ my_app/lib/user_profile.dart
@@ -1,24 +1,262 @@
 import 'package:flutter/material.dart';
+import './create_cards.dart';
+import './other_profiles.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePage();
-}
-
-class _HomePage extends State<HomePage> {
+class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: SingleChildScrollView(
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(4, 140, 139, 1),
+                Color.fromRGBO(102, 155, 139, 1),
+              ],
+              begin: FractionalOffset.bottomCenter,
+              end: FractionalOffset.topCenter,
+            ),
+          ),
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
             child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 34),
-                child: Column(children: [
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 73),
+              child: Column(
+                children: [
                   Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [Icon(Icons.add_circle_outline)])
-                ]))));
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      IconButton(
+                        Icons.add_circle_outline,
+                        color: Colors.white,
+                        onPressed: () async {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const CreateCards(),
+                            )
+                          )
+                        }
+                      ),
+                      IconButton(
+                        Icons.account_circle,
+                        color: Colors.white,
+                        onPressed: () async {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const OtherProfiles(),
+                            )
+                          )
+                        }
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'My Profile',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 34,
+                      
+                    ),
+                  ),
+                  SizedBox(
+                    height: 22,
+                  ),
+                  Container(
+                    height: height * 0.30,
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        double innerHeight = constraints.maxHeight;
+                        double innerWidth = constraints.maxWidth;
+                        return Stack(
+                          fit: StackFit.expand,
+                          children: [
+                            Positioned(
+                              bottom: 0,
+                              left: 0,
+                              right: 0,
+                              child: Container(
+                                height: innerHeight,
+                                width: innerWidth,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(30),
+                                  color: Colors.white,
+                                ),
+                                child: Column(
+                                  children: [
+                                    SizedBox(
+                                      height: 80,
+                                    ),
+                                    Text(
+                                      'Full Name',
+                                      style: TextStyle(
+                                        color: Colors.pink[200],
+                                        
+                                        fontSize: 37,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5,
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Column(
+                                          children: [
+                                            Text(
+                                              'Cards Made',
+                                              style: TextStyle(
+                                                color: Colors.grey[700],
+                                                
+                                                fontSize: 25,
+                                              ),
+                                            ),
+                                            Text(
+                                              '10',
+                                              style: TextStyle(
+                                                color: Colors.pink[200],
+                                                
+                                                fontSize: 25,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                            horizontal: 25,
+                                            vertical: 8,
+                                          ),
+                                          child: Container(
+                                            height: 50,
+                                            width: 3,
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(100),
+                                              color: Colors.grey,
+                                            ),
+                                          ),
+                                        ),
+                                        Column(
+                                          children: [
+                                            Text(
+                                              'Fav Topic',
+                                              style: TextStyle(
+                                                color: Colors.grey[700],
+                                                
+                                                fontSize: 25,
+                                              ),
+                                            ),
+                                            Text(
+                                              'Topic',
+                                              style: TextStyle(
+                                                color: Colors.pink[200],
+                                                
+                                                fontSize: 25,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                    Text(
+                                      'Bio',
+                                      style: TextStyle(
+                                        color: Colors.pink[200],
+                                        
+                                        fontSize: 25,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              top: 0,
+                              left: 0,
+                              right: 0,
+                              child: Center(
+                                child: Container(
+                                  width: 300,
+                                  height: 381,
+                                ),
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    height: height * 0.5,
+                    width: width,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            'My Cards',
+                            style: TextStyle(
+                              color: Colors.pink[200],
+                              fontSize: 27,
+                              
+                            ),
+                          ),
+                          Divider(
+                            thickness: 2.5,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            height: height * 0.15,
+                            decoration: BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            height: height * 0.15,
+                            decoration: BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
